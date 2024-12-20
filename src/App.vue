65bb76a652883900855a1ee7f3ce21f04e7d17bc
Edlith Vinueza <149545949+EdlithVinueza@users.noreply.github.com>
2024-12-19 19:44:58 -0500

    props

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Contador/>
-    <Contador/>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Contador encabezado="Contador Primero j" v-bind:valor="calcular(1, 3)" :esVerdad="muestra1" :valor2="78"/>
+  <Contador encabezado="Contador Segundo" v-bind:valor="calcular(5, 3)" v-bind:esVerdad="muestra2" />
+  <button @:click="cambiar(1)">Cambiar Primero</button>
+  <button @:click="cambiar(2)">Cambiar Segundo</button>
 </template>
 
+
 <script>
-
-// Importamos el componente
-import Contador  from './components/Contador.vue'
-
+import Contador from './components/Contador.vue';
 
 export default {
   name: 'App',
   components: {
-  Contador  //llamamos aqui al componente
-  }
+    Contador
+  },
+  data() {
+    return {
+      muestra1:true,
+      muestra2:true
+    };
+  },
+  methods: {
+    calcular(a, b) {
+      return a + b;
+    },
+    calcular2(a, b) {
+      return "HolaMundo";
+    },
+    calcular3() {
+      return ;
+    },
+    cambiar(valor) {
+      if (valor === 1) {
+        this.muestra1 = !this.muestra1;
+      }
+      if (valor === 2){
+      this.muestra2 = !this.muestra2;
+      }
+    }
+  },
+
 }
 </script>
+
 
 <style>
 #app {
--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,30 +1,30 @@
 <template>
-    <h2>{{ titulo }}</h2>
+    <h2>{{ encabezado }} : {{ valor2 }}</h2>
     <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputada }}</p>
     <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputada }}</p>
     <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputada }}</p>
     <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputada }}</p>
     <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputada }}</p>
+    
     <div>
-        <button v-on:click="incrementar()">+1</button>
-        <button v-on:click="decrementar()">-1</button>
+        <button @click="incrementar">+1</button>
+        <button v-on:click="decrementar">-1</button>
     </div>
-
+    <div v-if="esVerdad">
+        <h1>FELIZ Navidad</h1>
+    </div>
 </template>
 
 <script>
 export default {
+   
     data() {
         return {
-            numero: 10,
+            numero: this.valor,
             titulo: 'Contador'
         };
     },
     methods: {
-        calcularCuadrado() {
-            console.log('Entro a calcular');
-            return this.numero * this.numero;
-        },
         incrementar() {
             this.numero++;
         },
@@ -34,27 +34,52 @@ export default {
     },
     computed: {
         calcularCuadradoComputada() {
-            console.log('Entro a calcular computada ');
+            console.log('Entro a calcular computada');
             return this.numero * this.numero;
         }
-    }
+    },
+    props: {
+        encabezado: {
+            type: String,
+            //contega una j la palabra
+            validator(value){
+                return value.includes('j');
+            }
+        },
+        valor: Number,
+        valor2: {
+            type: Number,
+            Required:false,
+            default:77,
+            validator(value){
+                //programo mi validacion bajo mi criterio y retorno ture cuando es valido para mi
+                //retorno false cuando no es valido para mi
+                return value <= 77;
+            },
+        },
+        esVerdad: {
+            type:Boolean, 
+            Required:true,
+        },
+        arreglo:Array,
+        fecha: Date,
+        objetoPersona:Object,
+    },
 }
 </script>
 
 <style>
-
-button{
+button {
     background: #64B687;
     border-radius: 5px;
     border: 1px solid white;
-    color:white;
+    color: white;
     cursor: pointer;
-    margin: 0px 5px; /*arraba-abajo y derecha-izquierda*/
+    margin: 0px 5px; 
     padding: 5px 10px;
 }
 
-button:hover{
+button:hover {
     background: #5aa67b;
 }
-
 </style>
