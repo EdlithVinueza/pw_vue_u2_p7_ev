d14540c839c6985ce5b166272c0ad84728a920ec
Edlith Vinueza <149545949+EdlithVinueza@users.noreply.github.com>
2024-12-20 19:48:44 -0500

    consimir APIAs

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Contador encabezado="Contador Primero j" v-bind:valor="calcular(1, 3)" :esVerdad="muestra1" :valor2="78"/>
-  <Contador encabezado="Contador Segundo" v-bind:valor="calcular(5, 3)" v-bind:esVerdad="muestra2" />
-  <button @:click="cambiar(1)">Cambiar Primero</button>
-  <button @:click="cambiar(2)">Cambiar Segundo</button>
+  <JuegoPage />
 </template>
 
 
 <script>
-import Contador from './components/Contador.vue';
+import JuegoPage from './pages/JuegoPage.vue';
 
 export default {
   name: 'App',
   components: {
-    Contador
-  },
-  data() {
-    return {
-      muestra1:true,
-      muestra2:true
-    };
-  },
-  methods: {
-    calcular(a, b) {
-      return a + b;
-    },
-    calcular2(a, b) {
-      return "HolaMundo";
-    },
-    calcular3() {
-      return ;
-    },
-    cambiar(valor) {
-      if (valor === 1) {
-        this.muestra1 = !this.muestra1;
-      }
-      if (valor === 2){
-      this.muestra2 = !this.muestra2;
-      }
-    }
+    JuegoPage
   },
 
 }
 </script>
 
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Pregunta.vue
@@ -0,0 +1,43 @@
+<template>
+    <img :src="imagen" alt="No se puede ver">
+    <input v-model="pregunta" type="text" placeholder="Hazme una Pregunta">
+    <p>Recuerda que cuando finalices tu pregunta dar un ?</p>
+    <h1>{{ pregunta }}</h1>
+    <h2>{{ respuesta }}</h2>
+</template>
+
+<script>
+export default {
+    data() {
+        return { //Declaramos las propiedades reactivas
+            pregunta: 'Hola Mundo',
+            respuesta: null,
+            imagen: 'https://yesno.wtf/assets/yes/14-b57c6dc03aa15a4b18f53eb50d6197ee.gif'
+        }
+    },
+    watch: { //observadores de propiedades reactivas 
+        pregunta(value, oldValue) { //debe tener el mismo nombre de la propiedad reactiva a observar
+            console.log(value);
+            console.log(oldValue);
+            if (value.includes('?')) {
+                //llamar a ala API para obtener el si y el no
+                this. fachada();
+            }
+        }
+    },
+    methods:{
+        async llamarAPI(){ //metodo asincrono porel await
+            console.log('AQUI LLAMO AL API');
+            const data = await fetch('https://yesno.wtf/api').then(respuesta=>respuesta.json()); 
+            this.respuesta = data.answer;
+            this.imagen = data.image;
+            console.log(data);
+        },
+        async fachada(){  //metodo asincrono por el await
+            await this.llamarAPI();
+        }
+    }
+}
+</script>
+
+<style></style>
